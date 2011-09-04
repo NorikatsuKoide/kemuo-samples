b1ed2a36244257f6c6984021deb5189e38767ee6
kemumaki.kemuo <kemumaki.kemuo@gmail.com>
2011-09-04 06:36:10 +0000

    残念ながら諦めました。 このパズルは一筋縄ではいかないことが分かりました。 恐るべしパズル…

---
+++ gdd2011_quiz_slidepanel/src/constants.py
@@ -0,0 +1,13 @@
+#! -*- coding: utf-8 -*-
+'''
+Created on 2011/09/03
+
+@author: yohpapa
+'''
+
+# 定数定義
+DIR_UP			= 0
+DIR_DOWN		= 1
+DIR_LEFT		= 2
+DIR_RIGHT		= 3
+DIR_INVALID	= -1
--- gdd2011_quiz_slidepanel/src/panel_quiz.py
+++ gdd2011_quiz_slidepanel/src/panel_quiz.py
@@ -7,28 +7,54 @@ Created on 2011/08/31
 '''
 import sys
 from panel_engine import PanelEngine
+from logger import Logging
+import constants
 
-class MovingLimit:
+# 移動量管理
+class LimitManager:
 	def __init__(self, left, right, up, down):
 		self.up	= up
 		self.down	= down
 		self.left	= left
 		self.right	= right
 		
-	def is_over(self):
+	# 移動量を使い果たしたかチェックする
+	def isOver(self):
 		return self.up <= 0 or self.down <= 0 or self.left <= 0 or self.right <= 0
 
+	# 移動した分制限を更新する (消費する)
+	def update(self, dir):
+		if dir == constants.DIR_UP:
+			self.up -= 1
+		elif dir == constants.DIR_DOWN:
+			self.down -=1
+		elif dir == constants.DIR_LEFT:
+			self.left -= 1
+		elif dir == constants.DIR_RIGHT:
+			self.right -= 1
+	
+	# 指定方向へロールバックする
+	def rollback(self, dir):
+		if dir == constants.DIR_UP:
+			self.up += 1
+		elif dir == constants.DIR_DOWN:
+			self.down +=1
+		elif dir == constants.DIR_LEFT:
+			self.left += 1
+		elif dir == constants.DIR_RIGHT:
+			self.right += 1
+	
 # 移動制限値を取得する
-def get_move_limit(row):
+def __getMoveLimit(row):
 	limits = row.split(' ')
-	return MovingLimit(
+	return LimitManager(
 				int(limits[0]),
 				int(limits[1]),
 				int(limits[2]),
 				int(limits[3]))
 
 # ボードの数を取得する
-def get_num_board(row):
+def __getNumBoard(row):
 	return int(row)
 
 # 問題を解く
@@ -39,29 +65,32 @@ def solve_exercises(line, limits):
 	return str(solver)
 
 def main():
-	
 	if len(sys.argv) != 2:
 		return
 
-	file_name = sys.argv[1]
-	file = None
+	fileName = sys.argv[1]
+	file	  = None
 	try:
 		# ファイルから問題の条件などを取得
-		file = open(file_name, "r")
-		move_limit	= get_move_limit(file.readline())
-		num_board	= get_num_board(file.readline())
+		file		= open(fileName, "r")
+		limits		= __getMoveLimit(file.readline())
+		numBoard	= __getNumBoard(file.readline())
 		
-		print 'Start solving ' + str(num_board) + ' problems'
+		Logging.debug('Start solving ' + str(numBoard) + ' problems')
 		
 		# 問題を解くループ
+		noProblem = 1
 		for row in file:
-			solve = solve_exercises(row, move_limit)
-			if move_limit.is_over():
+			Logging.debug('Start solving No.%d problem' % noProblem)
+			solve = solve_exercises(row, limits)
+			if limits.isOver():
 				break
 			
-			print solve
+			# 問題の回答を出力
+			Logging.output(solve)
+			noProblem += 1
 		
-		print 'Solving ' + str(num_board) + ' problems finished'
+		Logging.debug('Solving ' + str(numBoard) + ' problems finished')
 		
 	finally:
 		if file != None:
