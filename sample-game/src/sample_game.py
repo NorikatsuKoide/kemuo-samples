d2abc5ae59e42c0455ac6e605a79b68144e838a7
kemumaki.kemuo <kemumaki.kemuo@gmail.com>
2011-09-29 15:11:31 +0000

    ソート対象のプロパティが二つある場合の実験

--- sample-game/src/sample_game.py
+++ sample-game/src/sample_game.py
@@ -28,7 +28,7 @@ class MainPage(webapp.RequestHandler):
 				<td><a href="/register">Register</td>
 			</tr>
 			<tr>
-				<td><a href="/query">Query</td>
+				<td><a href="/sort1">Sort1</td>
 			</tr>
 			</table>
 			</body>
@@ -128,9 +128,87 @@ class RegisterPage(webapp.RequestHandler):
 		player.put()
 		self.redirect('/register')
 
+class Sort1Page(webapp.RequestHandler):
+	def get(self):
+		order = self.request.GET.get('order')
+		if order == None:
+			order = ''
+		
+		self.response.headers['Content-Type'] = 'text/html'
+		body = u'''
+		<!doctype html>
+		<html lang="en">
+			<head>
+				<meta charset=utf-8>
+				<title>The page for Creating</title>
+				<link rel="stylesheet" href="/css/%s/sample.css" type="text/css" />
+			</head>
+			<body>
+				<p class="very_important">エンティティをlevelとscoreでソートします</p>
+				<img src="/images/%s/sample.png" />
+				<hr>
+		''' % (os.environ['CURRENT_VERSION_ID'],
+				os.environ['CURRENT_VERSION_ID'])
+
+		query = db.GqlQuery('''
+		SELECT * FROM Player ORDER BY level %s, score %s
+		''' % (order, order))
+		
+		body += u'''
+				<table border=1>
+					<tr>
+						<th class="label">名前</th>
+						<th class="label">レベル</th>
+						<th class="label">得点</th>
+						<th class="label">クラス</th>
+					<tr>
+		'''
+		
+		for player in query:
+			body += u'''
+					<tr align=center>
+						<td>%s</td>
+						<td>%d</td>
+						<td>%d</td>
+						<td>%s</td>
+					</tr>
+			''' % (player.name, player.level, player.score, player.charclass)
+			
+		asc = ''
+		desc = ''
+		if order == '':
+			asc = 'checked'
+		else:
+			desc = 'checked'
+		
+		body += u'''
+				</table>
+				<p>
+				<form method="post" action="/sort1">
+					<input type="radio" name="order" value="asc" %s>昇順
+					<input type="radio" name="order" value="desc" %s>降順
+					<input type="submit" value="ソートする">
+				</form>
+				<hr>
+				<a href="/">戻る</a>
+			</body>
+		</html>
+		''' % (asc, desc)
+		
+		self.response.out.write(body)
+		
+	def post(self):
+		url = '/sort1'
+		order = self.request.get('order')
+		if order == 'desc':
+			url += '?order=desc'
+			
+		self.redirect(url)
+			
 application = webapp.WSGIApplication(
 					[('/', MainPage),
-					 ('/register', RegisterPage)],
+					 ('/register', RegisterPage),
+					 ('/sort1', Sort1Page)],
 					debug=True)
 
 def main():
