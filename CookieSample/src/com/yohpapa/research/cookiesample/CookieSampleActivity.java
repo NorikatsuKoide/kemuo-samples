85ac3b584963e3dfd3dc1cbf2d398c3f13bcaaca
kemumaki.kemuo <kemumaki.kemuo@gmail.com>
2012-01-03 15:23:17 +0000

    Cookieを取得するサンプル作ってみた。

---
+++ CookieSample/src/com/yohpapa/research/cookiesample/CookieSampleActivity.java
@@ -0,0 +1,99 @@
+package com.yohpapa.research.cookiesample;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.Window;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+public class CookieSampleActivity extends Activity {
+
+	@SuppressWarnings("unused")
+	private static final String TAG = CookieSampleActivity.class.getSimpleName();
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+		setContentView(R.layout.main);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if(requestCode != 0) {
+			super.onActivityResult(requestCode, resultCode, data);
+			return;
+		}
+		
+		String cookieBuf = ((CookieSampleApp)getApplication()).restoreCookie();
+		if(cookieBuf == null) {
+			Toast.makeText(this, R.string.error_message, Toast.LENGTH_SHORT).show();
+			return;
+		}
+		
+		String[] cookies = cookieBuf.split(";");
+		
+		SimpleAdapter adapter = new SimpleAdapter(
+											this,
+											getListData(cookies),
+											R.layout.list_item,
+											new String[] {"LABEL", "CONTENT"},
+											new int[] {R.id.cookie_label, R.id.cookie_content});
+		
+		ListView list = (ListView)findViewById(R.id.cookie_list);
+		list.setAdapter(adapter);
+	}
+	
+	private List<Map<String, String>> getListData(String[] cookies) {
+		List<Map<String, String>> list = new ArrayList<Map<String,String>>();
+		
+		for(String cookie : cookies) {
+			String[] buf = cookie.trim().split("=");
+			list.add(getMapData(buf[0], buf[1]));
+		}
+		
+		return list;
+	}
+	
+	private Map<String, String> getMapData(String data1, String data2) {
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("LABEL", data1);
+		map.put("CONTENT", data2);
+		return map;
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		boolean result = false;
+		int itemId = item.getItemId();
+		switch(itemId) {
+		case R.id.menu_login:
+			Intent intent = new Intent(CookieSampleActivity.this, CookieSampleWebView.class);
+			startActivityForResult(intent, 0);
+			break;
+			
+		default:
+			result = super.onOptionsItemSelected(item);
+			break;
+		}
+		
+		return result;
+	}
+}
---
+++ CookieSample/src/com/yohpapa/research/cookiesample/CookieSampleWebView.java
@@ -0,0 +1,63 @@
+package com.yohpapa.research.cookiesample;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.webkit.CookieManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class CookieSampleWebView extends Activity {
+	private static final String TAG = CookieSampleWebView.class.getSimpleName();
+
+	private static final String LOGIN_URL = "https://accounts.google.com/ServiceLogin?hl=ja";
+	private static final String LOGIN_FINISH_URL = "https://plus.google.com/settings/";
+	
+	private WebView _webView = null;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+		setContentView(R.layout.login_form);
+		
+		_webView = (WebView)findViewById(R.id.login_webview);
+		_webView.setWebViewClient(_webViewClient);
+		_webView.getSettings().setJavaScriptEnabled(true);
+		_webView.loadUrl(LOGIN_URL);
+	}
+	
+	private final WebViewClient _webViewClient = new WebViewClient() {
+		@Override
+		public void onPageFinished(WebView view, String url) {
+			if(url.indexOf(LOGIN_FINISH_URL) != 0) {
+				if(!url.equals(LOGIN_URL)) {
+					_webView.setVisibility(View.INVISIBLE);
+				}
+				return;
+			}
+			
+			String cookies = CookieManager.getInstance().getCookie(LOGIN_URL);
+			if(cookies == null) {
+				Log.e(TAG, "Error: cookies is null.");
+				return;
+			}
+			
+			Log.v(TAG, "cookies: " + cookies);
+			
+			// TODO:
+			// Cookie情報をContentProviderに保存しておく
+			// その時、暗号化やセキュリティ設定を完璧にしておくこと
+			
+			((CookieSampleApp)getApplication()).storeCookie(cookies);
+			
+			// Cookieを取得したら元の画面に戻る
+			Intent intent = new Intent();
+			setResult(0, intent);
+			finish();
+		}
+	};
+}
